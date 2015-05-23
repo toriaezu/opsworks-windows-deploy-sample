0d6488e6cc2d560bab9fed61f4ed98d91b0c5223
https://github.com/settings/profile <lenadkn@gmail.com>
2015-05-23 02:42:46 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
 name 'opsworks-windows-deploy-sample'
 version '0.0.1'
-recipe 'opsworks-windows-deploy-sample::default', 'Default recipe'
-recipe 'opsworks-windows-deploy-sample::app_deploy', 'Recipe to deploy application'
+recipe 'opsworks-windows-deploy-sample::default', 'Recipe to install IIS'
+recipe 'opsworks-windows-deploy-sample::deploy', 'Recipe to deploy application'
